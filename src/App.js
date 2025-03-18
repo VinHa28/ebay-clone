a8434140ca205f783d7e4f5a17e93f317bd49927
vinhHa28 <vinhhv28@gmail.com>
2025-03-18 10:11:34 +0700

    Add images, edit database.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'
-import Home from './components/Home';
+import Checkout from './pages/Checkout';
 function App() {
   return (
     <div className="App">
       {/*Header*/}
       <Header></Header>
       {/*Home*/}
-      <Home></Home>
+      <Checkout></Checkout>
       {/*Footer*/}
       <Footer></Footer>
     </div>
